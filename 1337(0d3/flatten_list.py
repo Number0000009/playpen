348ecc3782c0402cc837590197bd2cc067d9e405
motherf <no.0000000000009@gmail.com>
2021-06-10 18:17:23 +0100

    Add unit tests for 'flatten list'

--- 1337(0d3/flatten_list.py
+++ 1337(0d3/flatten_list.py
@@ -2,6 +2,8 @@
 
 #TODO: recursion is dangerous
 
+import unittest
+
 def parse_one(input, output):
 	if isinstance(input, int):
 		output.append(input)
@@ -10,7 +12,37 @@ def parse_one(input, output):
 		for i in input:
 			parse_one(i, output)
 
-l = [1,2,3], [[[42]]], [[]], [[4], 5, 6], [[7, 8, 9]]
+
+class TestMethods(unittest.TestCase):
+	def test_one(self):
+		l = [1, 2, 3], [[[42]]], [[]], [[4], 5, 6], [[7, 8, 9]]
+		k = []
+		parse_one(l, k)
+		self.assertEqual(k, [1, 2, 3, 42, 4, 5, 6, 7, 8, 9])
+
+	def test_two(self):
+		l = [1, 2, 3]
+		k = []
+		parse_one(l, k)
+		self.assertEqual(k, [1, 2, 3])
+
+	def test_three(self):
+		l = []
+		k = []
+		parse_one(l, k)
+		self.assertEqual(k, [])
+
+	def test_four(self):
+		l = [], [1, 2, 3], [[]], [[4], 6, 5], [[9, 8, 7]]
+		k = []
+		parse_one(l, k)
+		self.assertEqual(k, [1, 2, 3, 4, 6, 5, 9, 8, 7])
+
+
+if __name__ == '__main__':
+	unittest.main()
+
+l = [1, 2, 3], [[[42]]], [[]], [[4], 5, 6], [[7, 8, 9]]
 k = []
 
 print("initial ", l)
@@ -19,7 +51,7 @@ parse_one(l, k)
 print("final")
 print(k)
 
-l = [1,2,3]
+l = [1, 2, 3]
 k = []
 
 print("initial ", l)
@@ -37,7 +69,7 @@ parse_one(l, k)
 print("final")
 print(k)
 
-l = [], [1,2,3], [[]], [[4], 5, 6], [[7, 8, 9]]
+l = [], [1, 2, 3], [[]], [[4], 6, 5], [[9, 8, 7]]
 k = []
 
 print("initial ", l)
