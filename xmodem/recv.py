d862530b1de1d80eed26ec955eb30b49542d5da5
no9 <no.0000000000009@gmail.com>
2021-07-23 17:33:50 +0100

    fixup! Add simple xmodem implementation
    Implement a proper loop

--- xmodem/recv.py
+++ xmodem/recv.py
@@ -1,24 +1,113 @@
+#!/usr/bin/env python3
+
+import sys
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class connect:
 
-s.connect(("127.0.0.1", 43967))
+	SOH = 1		# Start Of Header
+	EOT = 4		# End Of Transmission
+	ACK = 6		# Acknowledge
+	NAK = 0x15	# Not acknowledge
+	ETB = 0x17	# End of transmission Block
+	CAN = 0x18	# Cancel
+	REQ = b"C"
+	BLOCK_SIZE = 128
+	HEADER_SIZE = 3
+	FOOTER_SIZE = 2
 
-s.send(b"C")
+	def __init__(self, IP, PORT):
+		self.IP = IP
+		self.PORT = int(PORT)
 
-# SOH 01 255-01 data[128] crc-16[2]
-data = s.recv(128 + 2 + 1 + 1 + 1)
+		try:
+			self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			self.s.connect((self.IP, self.PORT))
+		except socket.error as ex:
+			print("Fatal error")
+			print(ex)
 
-print(data.hex())
 
-s.send(b"\x06")	# ACK
+	def __del__(self):
+		self.s.close()
 
-data = s.recv(128 + 2 + 1 + 1 + 1)
 
-print(data.hex())
+	def req(self):
+		print("REQ")
+		self.s.send(self.REQ)
 
-# 4 = EOT
 
-s.send(b"\x06")	# ACK
+	def ack(self):
+		print("ACK")
+		self.s.send(self.ACK.to_bytes(1, "big"))
 
-s.close()
+
+	def nak(self):
+		print("NAK")
+		self.s.send(self.NAK.to_bytes(1, "big"))
+
+
+	def crc(self, data):
+		crc = 0
+
+		for i in data[self.HEADER_SIZE:-self.FOOTER_SIZE]:
+
+			crc = crc ^ (i << 8)
+
+			for j in range(8):
+				if crc & 0x8000:
+					crc = crc << 1 ^ 0x1021
+				else:
+					crc = crc << 1
+
+		return crc & 0xffff
+
+
+	def read(self):
+		print("read")
+
+		self.req()
+
+		try:
+			while True:
+				# SOH 01 255-01 data[128] crc-16[2]
+				data = self.s.recv(self.BLOCK_SIZE + self.HEADER_SIZE + self.FOOTER_SIZE)
+
+				if data == self.EOT.to_bytes(1, "big"):
+					print("EOT")
+					self.ack()
+					return data_buf
+
+				crc = self.crc(data)
+				if crc.to_bytes(2, "big") == data[-self.FOOTER_SIZE:]:
+					self.ack()
+				else:
+					print("crc mismatch")
+					self.nak()
+
+				data_buf = data
+
+		except socket.error as ex:
+			print("Fatal error")
+			print(ex)
+
+
+if __name__ == "__main__":
+
+	if len(sys.argv) < 2:
+		print("Usage: recv.py PORT")
+		exit(1)
+
+	IP = "127.0.0.1"
+	PORT = sys.argv[1]
+
+	c = connect(IP, PORT)
+	if c is None:
+		print("Couldn't connect")
+		exit(1)
+
+	with open("xmodem_rcv.bin", "wb") as file:
+		data = c.read()
+		file.write(data[c.HEADER_SIZE:-c.FOOTER_SIZE])
+		file.close()
+
