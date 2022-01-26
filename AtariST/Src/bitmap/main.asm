1591d95d6a87a40cfc71bea9dabc124d608c2915
no9 <no.0000000000009@gmail.com>
2022-01-26 00:04:25 +0000

    Add DEGAS Elite bitmap screen viewer
    DEGAS Elite PI1 file is stripped of the first word,
    and the last 16 words.
    
    DEGAS Elite PI1 file format:
    DEGAS Elite     *.PI1 (low resolution)
                    *.PI2 (medium resolution)
                    *.PI3 (high resolution)
    
    1 word          resolution (0 = low res, 1 = medium res, 2 = high res)
                    Other bits may be used in the future; use a simple bit
                    test rather than checking for specific word values.
    16 words        palette
                    For high resolution pictures the least significant bit of
                    the first word determines whether the picture is black on
                    white (bit set) or white on black (bit not set)
    16000 words     picture data (screen memory)
    4 words         left color animation limit table (starting color numbers)
    4 words         right color animation limit table (ending color numbers)
    4 words         animation channel direction flag (0 = left, 1 = off, 2 = right)
    4 words         128 - animation channel delay in 1/60's of a second. [0 - 128]
                    (I.e., subtract word from 128 to get 1/60th's of a second.)
    -----------
    32066 bytes     total

---
+++ AtariST/Src/bitmap/main.asm
@@ -0,0 +1,66 @@
+; Get video mode
+	move.w #4,-(sp)
+	trap #14
+	addq.l #2,sp
+
+; Store it for later
+	move.w d0,previous_video_mode
+
+; Disable cursor
+	move.w #0,-(sp)		; not needed
+	move.w #0,-(sp)		; disable cursor
+	move.w #21,-(sp)
+	trap #14
+	addq.l #6,sp
+
+; Set 320x240x4 bitplans mode
+	move.w #0,-(sp)
+	move.l #-1,-(sp)
+	move.l #-1,-(sp)
+	move.w #5,-(sp)
+	trap #14
+	add.l #12,sp
+
+; Load palette
+	move.l #bitmap,-(sp)
+	move.w #6,-(sp)
+	trap #14
+	addq.l #6,sp
+
+; Read vram logical address via XBIOS
+	move.w #3,-(sp)
+	trap #14
+	addq.l #2,sp
+
+	move.l d0,a1		; Screen pointer
+
+	lea bitmap+(16*2),a0
+
+	move.w #(32000/4)-2,d0
+loop:
+	move.l (a0)+,(a1)+
+	dbra d0,loop
+
+; Wait for a key
+	move.w #7,-(sp)
+	trap #1
+	addq.l #2,sp
+
+; Restore resolution
+	move.w previous_video_mode,-(sp)
+	move.l #-1,-(sp)
+	move.l #-1,-(sp)
+	move.w #5,-(sp)
+	trap #14
+	add.l #12,sp
+
+; TODO: restore system palette
+
+; Exit
+	clr.w -(sp)
+	trap #1
+
+previous_video_mode: dw 0
+
+	align 4
+bitmap	incbin "dw.raw"
